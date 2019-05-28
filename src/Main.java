eb0448f0adde920821cee11cd7192184f3dbaff4
Krzysztof Giblewski <krzysztofgiblewski@gmail.com>
2019-05-28 20:24:03 +0200

    najprostrzy kwadrat

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+public class Main {
+
+    public static void main(String[] args) {
+        String znaczek;
+        znaczek = "*";
+
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 1; j <= 5; j++) {
+                znaczek = "   ";
+                if (i == 1 || i == 5) znaczek = " * ";
+                if (j == 1 || j == 5) znaczek = " * ";
+                if (j == 2 && i == 2 || j == 4 && i == 2) znaczek = " * ";
+                if (j == 3 && i == 3 || j == 3 && i == 3) znaczek = " * ";
+                if (j == 2 && i == 4 || j == 4 && i == 4) znaczek = " * ";
+                System.out.print(znaczek);
+                if (j == 5) System.out.println("\n");
+            }
+
+        }
+
+    }
+}
+
