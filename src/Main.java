c1ee0c48e33c85022b9d7127672c1a82d7ad8bb1
Krzysztof Giblewski <krzysztofgiblewski@gmail.com>
2019-05-30 12:20:47 +0200

    wyciagnięte stałe

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,22 @@
 import java.util.Scanner;
 
 public class Main {
+    public static String gwiazdka = " * ";
+    public static String spacja = "   ";
 
     public static void main(String[] args) {
         String znaczek;
-        znaczek = "*";
         int dlugoscBokow = 21;
         System.out.println("Podaj z ilu * ma składać się kwadrat");
         Scanner scanner = new Scanner(System.in);
         dlugoscBokow = scanner.nextInt();
         for (int i = 1; i <= dlugoscBokow; i++) {
             for (int j = 1; j <= dlugoscBokow; j++) {
-                znaczek = "   ";
-                if (i == 1 || i == dlugoscBokow) znaczek = " * "; //gorny bok
-                if (j == 1 || j == dlugoscBokow) znaczek = " * ";  //dolny bok
-                if (j == i) znaczek = " * "; //przekatna
-                if (j == dlugoscBokow - i + 1) znaczek = " * "; //druga przekatna
+                znaczek = spacja;
+                if (i == 1 || i == dlugoscBokow) znaczek = gwiazdka; //gorny bok
+                if (j == 1 || j == dlugoscBokow) znaczek = gwiazdka;  //dolny bok
+                if (j == i) znaczek = gwiazdka; //przekatna
+                if (j == dlugoscBokow - i + 1) znaczek = gwiazdka; //druga przekatna
                 System.out.print(znaczek);
                 if (j == dlugoscBokow) System.out.println("\n");
             }
