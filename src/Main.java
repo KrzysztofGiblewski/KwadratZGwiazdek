7dabe4499d2479952e9061c024ab91f5e367188a
Krzysztof Giblewski <krzysztofgiblewski@gmail.com>
2019-05-30 11:35:25 +0200

    teraz ladnie rysuje kwadrat o dowolnych bokach

--- src/Main.java
+++ src/Main.java
@@ -3,20 +3,17 @@ public class Main {
     public static void main(String[] args) {
         String znaczek;
         znaczek = "*";
+        int dlugoscBokow = 21;
 
-        for (int i = 1; i <= 5; i++) {
-            for (int j = 1; j <= 5; j++) {
+        for (int i = 1; i <= dlugoscBokow; i++) {
+            for (int j = 1; j <= dlugoscBokow; j++) {
                 znaczek = "   ";
-                if (i == j) znaczek = " * ";
-                if (i == 1 || i == 5) znaczek = " * ";
-                if (j == 1 || j == 5) znaczek = " * ";
-                if (j == i || j - i == i) znaczek = " * ";
-                if (i - j == j) znaczek = " * ";
-//                if (j == 2 && i == 2 || j == 4 && i == 2) znaczek = " * ";
-//                if (j == 3 && i == 3 || j == 3 && i == 3) znaczek = " * ";
-//                if (j == 2 && i == 4 || j == 4 && i == 4) znaczek = " * ";
+                if (i == 1 || i == dlugoscBokow) znaczek = " * "; //gorny bok
+                if (j == 1 || j == dlugoscBokow) znaczek = " * ";  //dolny bok
+                if (j == i) znaczek = " * "; //przekatna
+                if (j == dlugoscBokow - i + 1) znaczek = " * "; //druga przekatna
                 System.out.print(znaczek);
-                if (j == 5) System.out.println("\n");
+                if (j == dlugoscBokow) System.out.println("\n");
             }
 
         }
