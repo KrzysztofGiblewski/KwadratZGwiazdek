0a83a7a3dd64a1c22047521e1b6569ed98a46be3
Krzysztof Giblewski <krzysztofgiblewski@gmail.com>
2019-05-30 11:09:25 +0200

    zmiana w ifach na bardziej uniwersalną

--- src/Main.java
+++ src/Main.java
@@ -7,11 +7,14 @@ public class Main {
         for (int i = 1; i <= 5; i++) {
             for (int j = 1; j <= 5; j++) {
                 znaczek = "   ";
+                if (i == j) znaczek = " * ";
                 if (i == 1 || i == 5) znaczek = " * ";
                 if (j == 1 || j == 5) znaczek = " * ";
-                if (j == 2 && i == 2 || j == 4 && i == 2) znaczek = " * ";
-                if (j == 3 && i == 3 || j == 3 && i == 3) znaczek = " * ";
-                if (j == 2 && i == 4 || j == 4 && i == 4) znaczek = " * ";
+                if (j == i || j - i == i) znaczek = " * ";
+                if (i - j == j) znaczek = " * ";
+//                if (j == 2 && i == 2 || j == 4 && i == 2) znaczek = " * ";
+//                if (j == 3 && i == 3 || j == 3 && i == 3) znaczek = " * ";
+//                if (j == 2 && i == 4 || j == 4 && i == 4) znaczek = " * ";
                 System.out.print(znaczek);
                 if (j == 5) System.out.println("\n");
             }
